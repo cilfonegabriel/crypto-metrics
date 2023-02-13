c946e0213a32882fa51d9e28ec7dd3fa9b8a577e
gabicilfone <cilfonegabriel@gmail.com>
2023-02-13 15:17:41 -0300

    Modify react directory structure, add new files

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,7 +1,8 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
+    import('web-vitals').then(({
+      getCLS, getFID, getFCP, getLCP, getTTFB,
+    }) => {      getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
