0b9f2b6c822ae316256a85d53777efc7f5f63b6e
gabicilfone <cilfonegabriel@gmail.com>
2023-02-14 14:59:33 -0300

    add Header in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CoinsList from './components/CoinsList';
-import CoinDetailsPage from './components/CoinDetails';
+import CoinDetails from './components/CoinDetails';
+import Header from './components/Header';
+
 
 const App = () => (
   <Router>
+    <Header />
     <Routes>
       <Route path="/" element={<CoinsList />} />
-      <Route path="/details/:id/" element={<CoinDetailsPage />} />
+      <Route path="/details/:id/" element={<CoinDetails />} />
     </Routes>
   </Router>
 );
